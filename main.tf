e06cb25507a29e4176a03b6f70795d78cf33ec76
lordz-md <lordz.md@gmail.com>
2019-11-26 15:48:37 +0200

    Updated backend info

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 # Require TF version to be same as or greater than 0.12.13
 terraform {
   required_version = ">=0.12.13"
-  #backend "s3" {
-  #  bucket         = "kyler-github-actions-demo-terraform-tfstate"
-  #  key            = "terraform.tfstate"
-  #  region         = "us-east-1"
-  #  dynamodb_table = "aws-locks"
-  #  encrypt        = true
-  #}
+  backend "s3" {
+  bucket         = "kyler-github-actions-demo-terraform-tfstate"
+  key            = "terraform.tfstate"
+  region         = "us-east-1"
+  dynamodb_table = "aws-locks"
+  encrypt        = true
+  }
 }
 
 # Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
